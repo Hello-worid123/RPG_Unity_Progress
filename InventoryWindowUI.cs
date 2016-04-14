8519120fe36a6c63e23a338061913eb9fa75a98e
xiao-pan <18316974251@163.com>
2016-04-14 23:58:34 +0800

    commit code

---
+++ InventoryWindowUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryWindowUI : MonoBehaviour {
+
+   private UIButton _InventoryWindowCloseBtn;
+
+	// Use this for initialization
+
+   void Awake() {
+       _InventoryWindowCloseBtn = transform.Find("Background/Close Button").GetComponent<UIButton>();
+   }
+	void Start () {
+
+        EventDelegate ed5 = new EventDelegate(this, "OnHideInventoryWindow");
+        _InventoryWindowCloseBtn.onClick.Add(ed5);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnHideInventoryWindow()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
