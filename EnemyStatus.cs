ebe8234234082d7bd33846a91f5ebd84561b72cc
xiao-pan <18316974251@163.com>
2016-04-14 23:54:46 +0800

    commit code

---
+++ EnemyStatus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStatus : MonoBehaviour{
+
+    public string _enemyName;//怪物名称
+    public int _enemyID;//怪物ID
+
+    [System.Serializable]
+
+    public class EnemyAttribute {
+        public int Lv, Hp, Mp, Atk, Def, Spd;//等级，生命，法力，攻击，防御，速度（影响攻击速度和移动速度）
+        public float HitRate, CriticalRate, MissRate, AtkSpd, AtkRange, Movespd;//命中率，暴击率，闪避率，攻击速度，攻击范围，移动速度
+    
+    }
+
+    public EnemyAttribute _enemyStatus;
+    public float _expGive;
+
+
+	
+}
