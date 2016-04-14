d17155336f9aee9013293a017b201af563551d3e
xiao-pan <18316974251@163.com>
2016-04-14 23:59:28 +0800

    commit code

---
+++ SkillParameters.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillParameters : MonoBehaviour {
+    [HideInInspector]
+    public float _multiDamage;
+    [HideInInspector]
+    public float _targetHitRate;
+    [HideInInspector]
+    public float _flichRate;
+    [HideInInspector]
+    public GameObject _skillOwner;
+
+    public void RecieveSkillParameters(float multidamage,float targetHitRate,float flichRate,GameObject skillowner) {
+        _multiDamage = multidamage;
+        _targetHitRate = targetHitRate;
+        _flichRate = flichRate;
+        _skillOwner = skillowner;
+    }
+}
