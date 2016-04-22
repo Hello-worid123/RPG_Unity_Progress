57010b51044b7a7ce4e96aa6b3a62d7d68d4b14f
pook123 <1043017207@qq.com>
2016-04-22 09:57:11 +0800

    commit code

---
+++ DetectArea.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectArea : MonoBehaviour {
+    [HideInInspector]
+    public EnemyAIController enemyAIcontroller;
+    public float Radius;
+    private CharacterController enemycharactercontroller;
+
+    public bool drawGizmos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        enemyAIcontroller = transform.parent.GetComponent<EnemyAIController>();
+        enemycharactercontroller = transform.parent.GetComponent<CharacterController>();
+        SphereCollider collider = this.GetComponent<SphereCollider>();
+        collider.center = enemycharactercontroller.center;
+        collider.radius = Radius;
+        collider.isTrigger = true;
+    }
+
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "PlayerN")
+        {
+            enemyAIcontroller.chaseTarget = true;
+            enemyAIcontroller.targetPlayer = c.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "PlayerN")
+        {
+        
+            enemyAIcontroller.chaseTarget = false;
+            enemyAIcontroller.targetPlayer = null;
+        }
+    }
+
+    //Draw Gizmoz
+    [ExecuteInEditMode]
+    void OnDrawGizmosSelected()
+    {
+        if (drawGizmos)
+        {
+            Gizmos.color = new Color(1f, 0f, 0f, 0.3f);
+            Gizmos.DrawSphere(transform.position, this.GetComponent<SphereCollider>().radius);
+        }
+    }
+
+
+
+
+
+}
